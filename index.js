bcd11e343afd3b17aa5346aa7d1f21a4ece30ca5
Nathaniel Inman <nate@theoestudio.com>
2020-08-11 16:00:46 -0500

    now exporting sectormap for custom map extends

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export {Map} from './Map';
 export {Sector} from './Sector';
+export {SectorMap} from './SectorMap';
 export {maps} from './maps/';
 export {BasicWebGLDisplay} from './BasicWebGLDisplay';
 export {BasicDisplay} from './BasicDisplay';
